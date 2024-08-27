3221fe0bdc00fe748f653dfc1c5185fa83c8e4c0
engrkamrantaj <65710098+engrkamrantaj@users.noreply.github.com>
2024-08-27 22:26:20 +0500

    first commit

---
+++ as7.py
@@ -0,0 +1,33 @@
+name = input("Enter your name = ")
+n1 = int(input("Enter your first fav number = "))
+n2 = int(input("Enter your second fav number = "))
+n3 = int(input("Enter your third fav number = "))
+
+print(f"\nHello {name} lets explore your fav numbers :")
+numbers = []
+numbers.append(n1)
+numbers.append(n2)
+numbers.append(n3)
+
+print(numbers)
+
+for x in numbers:
+    if x % 2 == 0:
+        print(f"The number {x} is even")
+    else:
+        print(f"The number {x} is odd")
+
+for x in numbers:
+    sqr = (x, x*x)
+    print(f"The number {x} and its square : {sqr}")
+
+n = n1+n2+n3
+print(f"Amazing! The sum of your favorite numbers is: {n}")
+
+for a in range(2, n):
+    if n % a == 0:
+        print(f"Wow {n} is not a prime number")
+        break
+    else:
+        print(f"Wow {n} is a prime number")
+        break
